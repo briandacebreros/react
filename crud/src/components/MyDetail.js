ed3fd4fd700232c4a6e38adc85ece14e2f506bb4
Carlos <carlopolo@gmail.com>
2020-11-01 12:15:37 +0000

    The detail shows now the actual detail from quavitam

--- crud/src/components/MyDetail.js
+++ crud/src/components/MyDetail.js
@@ -2,6 +2,9 @@ import React from 'react';
 import dateFormat from 'dateformat';
 import ReactDOM from 'react-dom';
 //import { NavLink } from 'react-router-dom';
+import {Button, Container, Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class MyDetail extends React.Component {
 
@@ -12,23 +15,37 @@ class MyDetail extends React.Component {
     	    //id:props.match.params.id,
             //isLoaded: false,
             //error: null,
-            els: []
+            el: {}
     };
   }
 
   componentDidMount() {
-	//this.setState({ els: []});
+	let url=`https://logisticsapi.roclas.com:3002/proxy/getExpedicion?idexpedicion=${this.props.match.params.id}`;
+	fetch(url, {method:'GET',
+                headers: {'Authorization': 'Basic ' + 
+                        btoa('reactApp:astooDificultToGuess-9F7-9u7-9.!.4456')}})
+        .then(response => response.json())
+        .then(json => this.setState({el:json.expediciones.reduce((a,b)=>b,{})}));
   }
 
   render(){
-    console.log("render props",this.props.match.params.id);
+    let el=this.state.el;
     return (
        <div>
-        <h1>Detail</h1>
-        <p>Detail page</p>
-        <p>
-	    This is the detail of element {this.props.match.params.id}
-        </p>
+        <h1>Detail {el.idexpedicion}</h1>
+        <p>Detail page for delivery {el.idexpedicion}</p>
+	This is the detail of element {this.props.match.params.id}
+	    <form>
+	    <Container><Row>
+	    {Object.entries(el).map(e => 
+		    <Col lg={6} sm={6} md={6}> {e[0]} <input name={e[0]} value={e[1]}/> </Col>
+	    )}
+	    </Row>
+	    </Container>
+	      <Button variant="primary" size="lg" type="submit" block>
+    		enviar
+  	      </Button>
+	    </form>
        </div>
     );
   }
